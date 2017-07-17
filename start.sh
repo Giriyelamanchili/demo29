680726036c84460eadd9423e432389ded43a5f8c
soumyakbhattacharyya <bhattacharyya.soumyak@gmail.com>
2017-07-18 02:45:05 +1000

    mapped host file system to jenkins home

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-docker run --publish 8080:8080 --publish 50000:50000 jenkins
+docker run -u root --publish 8080:8080 --publish 50000:50000 -v /home/vagrant/demo29/docker_jenkins_home/demo29:/var/jenkins_home jenkins
