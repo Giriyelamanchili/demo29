3ad30064e1a526df78557a72169693749dff6a24
soumyakbhattacharyya <bhattacharyya.soumyak@gmail.com>
2017-07-18 04:14:03 +1000

    install plugin

--- start.sh
+++ start.sh
@@ -1,1 +1,10 @@
-docker run -u root --publish 8080:8080 --publish 50000:50000 -v /home/vagrant/demo29/docker_jenkins_home/demo29:/var/jenkins_home jenkins
+#!/bin/bash
+# Run the container for testing.
+docker run --name jenkins-with-plugin \
+  -v "$(pwd)/jhome/jobs":/var/jenkins_home/jobs \
+  -v "$(pwd)/jhome/code":/var/jenkins_home/code \
+  -p 8080:8080 \
+  -p 50000:50000 \
+  --env JENKINS_SLAVE_AGENT_PORT=50000 \
+  --env JENKINS_OPTS=--httpPort=8080 \
+  with-plugin
