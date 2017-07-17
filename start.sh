57d5b1846c53e958a252ab6dc81c2ce73c1a4157
soumyakbhattacharyya <bhattacharyya.soumyak@gmail.com>
2017-07-18 03:10:11 +1000

    execute groovy script at start up

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-docker run -u root --publish 8080:8080 --publish 50000:50000 -v /home/vagrant/demo29/docker_jenkins_home/demo29:/var/jenkins_home jenkins
+docker run -u root --publish 8080:8080 --publish 50000:50000 -v /home/vagrant/demo29/docker_jenkins_home/demo29/jhome:/var/jenkins_home extra-executors
